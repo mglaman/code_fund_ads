12f0c8fb1df6571bb879e02d2c3485c79e71084f
Matt Glaman <nmd.matt@gmail.com>
2019-06-20 14:13:43 -0500

    Fix migration constant

--- db/migrate/20190619201904_update_property_revenue_percentage.rb
+++ db/migrate/20190619201904_update_property_revenue_percentage.rb
@@ -1,4 +1,4 @@
-class AddPropertyRevenuePercentage < ActiveRecord::Migration[5.2]
+class UpdatePropertyRevenuePercentage < ActiveRecord::Migration[5.2]
   def change
     change_column :properties, :revenue_percentage, :decimal, null: false, default: 0.6
   end
